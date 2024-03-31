eab7934f651abf67a12ea4bb553b7743d5334c87
CodexPremiera <ashleyken.professional@gmail.com>
2024-03-31 18:04:22 +0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,26 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        black_main: "#242424",
+        gray_text: "#6B6B6B",
+        banner: "rgb(255, 192, 23)",
+        green_custom: "rgb(26, 137, 23)",
+        green_hover: "#156D12"
+      },
+      fontFamily: {
+        title: `gt-super, Georgia, Cambria,Times New Roman, Times, serif;`,
+        texts: `sohne, Helvetica Neue, Helvetica, Arial, sans-serif`,
+      },
+      gridTemplateColumns: {
+        card: "repeat(auto-fit, minmax(280px, 1fr))",
+      }
+    },
+  },
+  plugins: [],
+}
